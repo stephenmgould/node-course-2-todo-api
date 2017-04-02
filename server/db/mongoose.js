4d1835fff3ce8797f6308bcf4cfee0044552f91a
Muirhead-Gould <smuirhe1@ca.corp.zayo.com>
2017-04-02 10:05:07 -0400

    Set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+//mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds147920.mlab.com:47920/tododatabase' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://stephenmgould:Piper123@ds147920.mlab.com:47920/tododatabase' || 'mongodb://localhost:27017/TodoApp');
 module.exports = {mongoose};
