df4a433ad83bd7a8c7083aabf1e2b5934a6795b5
Muirhead-Gould <smuirhe1@ca.corp.zayo.com>
2017-04-02 17:41:31 -0400

    setup separate test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect( 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://stephenmgould:Piper123@ds147920.mlab.com:47920/tododatabase' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://stephenmgould:Piper123@ds147920.mlab.com:47920/tododatabase');
 module.exports = {mongoose};
