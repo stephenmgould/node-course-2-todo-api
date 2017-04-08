e47f1a0675f74ff3f42e80175dce8f8cf5c5a997
Muirhead-Gould <smuirhe1@ca.corp.zayo.com>
2017-04-08 17:07:59 -0400

    add POST / users route and token method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect( 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://stephenmgould:Piper123@ds147920.mlab.com:47920/tododatabase');
+mongoose.connect( 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://stephenmgould:Piper123@ds147920.mlab.com:47920/tododatabase');
+//mongoose.connect(process.env.MONGODB_URI);
 module.exports = {mongoose};
